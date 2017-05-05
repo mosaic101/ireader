5ae6e3de23c613ca3ad3156baca1b8170bea3b0f
jianjin.wu <mosaic101@foxmail.com>
2017-05-05 11:29:16 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
@@ -15,8 +14,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
