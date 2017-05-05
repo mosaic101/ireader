789bf75c0f495f63d57cb20366a50c057712caad
jianjin.wu <mosaic101@foxmail.com>
2017-05-05 18:17:38 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Intro
-- a react demo based on  [create-react-app](https://github.com/facebookincubator/create-react-app)
 - created on 2017/5/5
+- a react demo based on  [create-react-app](https://github.com/facebookincubator/create-react-app)
+- plan to write a reader
+
 
 # Features
+- [Ant Design](https://github.com/ant-design/ant-design/blob/master/README-zh_CN.md)
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap-theme.css'
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
-import './index.css'
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <App />,
