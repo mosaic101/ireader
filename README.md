a35c81a04437a209974d44194cbf065fd400f97a
mosaic101 <mosaic101@foxmail.com>
2017-05-06 01:07:11 +0800

    temp

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Intro
 - created on 2017/5/5
-- a react demo based on  [create-react-app](https://github.com/facebookincubator/create-react-app)
+- a react demo based on react
 - plan to write a reader
 
 
 # Features
+- [create-react-app](https://github.com/facebookincubator/create-react-app)
 - [Ant Design](https://github.com/ant-design/ant-design/blob/master/README-zh_CN.md)
+
 
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react'
-import { DatePicker, message } from 'antd'
+import { DatePicker, message, Button } from 'antd'
 
 class Text extends Component {
   constructor(props) {
@@ -13,11 +13,13 @@ class Text extends Component {
     message.info('您选择的日期是: ' + date.toString())
     this.setState({ date })
   }
+  
   render() {
     return (
       <div style={{ width: 400, margin: '100px auto' }}>
         <DatePicker onChange={value => this.handleChange(value)} />
         <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
+        <Button type="dashed">Dashed</Button>
       </div>
     )
   }
