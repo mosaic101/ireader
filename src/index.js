93b4e1f8414562823f2fb82d41f8d521e5877888
jianjin.wu <mosaic101@foxmail.com>
2017-05-05 15:01:18 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css'
 
 ReactDOM.render(
   <App />,
   document.getElementById('app')
-);
+)
