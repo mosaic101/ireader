cdd8973242e060a84e6b43eaf4de8e2de7a9800d
mosaic101 <mosaic101@foxmail.com>
2017-05-12 00:00:12 +0800

    temp

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,20 @@
 
 import React, { Component } from 'react'
 import { Grid, WhiteSpace, WingBlank } from 'antd-mobile'
+import test from '../../test.png'
 
-const data = Array.from(new Array(18)).map((_val, i) => ({
-  icon: 'https://os.alipayobjects.com/rmsportal/wIjMDnsrDoPPcIV.png',
+const data = Array.from(new Array(9)).map((_val, i) => ({
+  icon: test,
   text: `name${i}`
 }))
 
 const renderItem = (dataItem, index) => (
   <div style={{ margin: '0.16rem', background: '#f7f7f7', textAlign: 'center' }}>
+    
+    <img src={dataItem.icon} style={{ width: '80%',height: '200', margin: '0.12rem' }} />
     <div style={{ background: 'rgba(0, 0, 0, 0.1)', padding: '0.08rem' }}>
       <span>{index + 1}.{dataItem.text}</span>
     </div>
-    <img src={dataItem.icon} style={{ width: '80%', margin: '0.12rem' }} />
   </div>
 )
 
@@ -24,6 +26,7 @@ class Body extends Component {
         <WingBlank size="lg">
           <Grid data={data} columnNum={3} hasLine={false}
             renderItem={renderItem}
+            style={{ background: '#f5f5f9' }}
             onClick={() => console.log('grid')}
           />
         </WingBlank>
